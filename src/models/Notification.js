a46b626ee506adba9335c97ef451432a48c79782
zaengetsu <106625404+zaengetsu@users.noreply.github.com>
2024-03-25 10:21:29 +0100

    correction du return du model Notification en Notifications

--- src/models/Notification.js
+++ src/models/Notification.js
@@ -39,5 +39,5 @@ export default (db) => {
             timestamps: true,
             paranoid: true // This will add a "deletedAt" column and allow soft deletes
         });
-        return Notification ;
+        return Notifications ;
 }

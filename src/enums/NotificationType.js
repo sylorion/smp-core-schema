3447445a309410f360db14e996affc8e0e6f978e
zaengetsu <106625404+zaengetsu@users.noreply.github.com>
2024-03-26 11:28:28 +0100

    creation de l'enum NotificationType et du model NotificationTemplate

---
+++ src/enums/NotificationType.js
@@ -0,0 +1,10 @@
+const NotificationType  = {
+    SMS: 'sms',
+    PUSH: 'push',
+    MAIL: 'mail',
+    FRONT: 'front',
+    
+  };
+  
+  export default NotificationType ;
+  
---
+++ src/models/NotificationTemplate.js
@@ -0,0 +1,42 @@
+
+// ./src/models/NotificationTemplate.js
+import { BaseEntityMixin } from '../mixins/BaseEntityMixin.js'
+import { DataTypes, Model } from 'sequelize'
+// TODO Create NotificationTemplateCategory and import it
+
+export default (db) => {
+    class NotificationTemplates extends BaseEntityMixin(Model) { };
+
+    NotificationTemplates.init(db, DataTypes, 
+        {
+        notificationTemplateID: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        // TODO:  update to NotificationTemplateType which will allow to infer a title
+        title: {
+            type: DataTypes.STRING(255),
+            allowNull: false
+        },
+        // TODO: update to messageTemplateID which will help to built messages
+        message: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        name: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+     
+    },
+        {
+            sequelize: db,
+            modelName: 'NotificationTemplate',
+            tableName: 'NotificationTemplate',
+            timestamps: true,
+            paranoid: true // This will add a "deletedAt" column and allow soft deletes
+        });
+        return NotificationTemplates ;
+}

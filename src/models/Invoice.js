4d8b796f81fb2e8c791439846d1a246f4824c7c8
zaengetsu <106625404+zaengetsu@users.noreply.github.com>
2024-08-16 11:59:17 +0200

    add three new columns , the selller organization who is the organization who creat the service et propose it , the buyerOrganization in the case ou this is a organization who decide to buy the service and the authorID to bind the invoice to the user

--- src/models/Invoice.js
+++ src/models/Invoice.js
@@ -1,62 +1,75 @@
 // src/models/Invoice.js
 // Use ES6 exporter to the outer and not CommonJS
-import { BaseEntityMixin } from '../mixins/BaseEntityMixin.js'
-import { DataTypes, Model } from 'sequelize'
-// Importing enums 
-import PaymentStatus from '../enums/PaymentStatus.js';
+import { BaseEntityMixin } from "../mixins/BaseEntityMixin.js";
+import { DataTypes, Model } from "sequelize";
+// Importing enums
+import PaymentStatus from "../enums/PaymentStatus.js";
 
 export default (db) => {
+  class Invoice extends BaseEntityMixin(Model) {}
 
-  class Invoice extends BaseEntityMixin(Model) { };
-
-  Invoice.init(db, DataTypes, 
+  Invoice.init(
+    db,
+    DataTypes,
     {
-    invoiceID: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    estimateID: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
+      invoiceID: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      authorID: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      estimateID: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      buyerOrganizationID: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
 
-    thirdPartyFees: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
+      sellerOrganizationID: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      thirdPartyFees: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      servicesFees: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      servicesVatPercent: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      prestationsVatPercent: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      totalAmount: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      paymentStatus: {
+        type: DataTypes.ENUM(Object.values(PaymentStatus)),
+        defaultValue: PaymentStatus.SUBMITTED,
+      },
+      emitDate: DataTypes.DATE,
+      dueDate: DataTypes.DATE,
+      digitalSignature: DataTypes.TEXT,
     },
-    servicesFees: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    servicesVatPercent: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    prestationsVatPercent: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    totalAmount: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    paymentStatus: {
-      type: DataTypes.ENUM(Object.values(PaymentStatus)),
-      defaultValue: PaymentStatus.SUBMITTED,
-    },
-    emitDate: DataTypes.DATE,
-    dueDate: DataTypes.DATE,
-    digitalSignature: DataTypes.TEXT, 
-  },
-  {
-    sequelize: db,
-    modelName: 'Invoice',
-    tableName: 'Invoice',
-    timestamps: true,
-    paranoid: true,
-  });
+    {
+      sequelize: db,
+      modelName: "Invoice",
+      tableName: "Invoice",
+      timestamps: true,
+      paranoid: true,
+    }
+  );
 
   return Invoice;
-
-}
+};

3260bb239e68e31ae185e4b516606d4b3a813a93
J. Charles N. M <jcnm@sylorion.com>
2024-10-17 17:08:46 +0200

    Delete unusefull slug and uuid function from basemixins

--- src/mixins/BaseEntityMixin.js
+++ src/mixins/BaseEntityMixin.js
@@ -3,21 +3,6 @@
 import ObjectStatus from '../enums/ObjectStatus.js';
 import { v4 as uuidv4 } from 'uuid';
 import slugify from 'slugify';
- 
-function slug(from) {
-    return slugify(from, {
-        replacement: '-',  // replace spaces with replacement character, defaults to `-`
-        remove: undefined, // remove characters that match this regex, let to the defaults `undefined`
-        lower: true,      // convert to lower case, defaults to `false`
-        strict: false,     // strip special characters except replacement, defaults to `false`
-        locale: 'en',      // language code of the locale to use
-        trim: true         // trim leading and trailing replacement chars, defaults to `true`
-    })
-}
-
-function uuid() {
-    return uuidv4();
-}
 
 function BaseEntityMixin(GivenModel) {
     return class extends GivenModel {
@@ -34,6 +19,7 @@ function BaseEntityMixin(GivenModel) {
         slug(from, options){
             return slugify(from, options)
         }
+        
         uuid(){
             return uuidv4();
         }

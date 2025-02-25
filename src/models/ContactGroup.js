83a1bbb2bd8064b8f1e93c68061e6c13f039c9ee
zaengetsu <106625404+zaengetsu@users.noreply.github.com>
2025-02-25 18:23:31 +0100

    cette table permet de sequencer les differents contacts en plusieurs criteres au besoin , on peut faire un groupe par pays , par sexe , par age afin d'avoir des cannaux précis au possible pour cibler durant les campagnes

---
+++ src/models/ContactGroup.js
@@ -0,0 +1,52 @@
+import { BaseEntityMixin } from "../mixins/BaseEntityMixin.js";
+import { DataTypes, Model } from "sequelize";
+import ObjectStatus from "../enums/ObjectStatus.js";
+
+export default (db) => {
+  class ContactGroup extends BaseEntityMixin(Model) {}
+
+  ContactGroup.init(
+    {
+    contactGroupID: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      uniqRef: {
+        type: DataTypes.STRING(36),
+        allowNull: false,
+      },
+      slug: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: true, // Remplace legend pour donner une description claire du groupe
+      },
+      criteria: {
+        type: DataTypes.JSONB,
+        allowNull: true, // Ex. : { "country": "France", "gender": "female", "minAge": 25 }
+      },
+      state: {
+        type: DataTypes.ENUM(...Object.values(ObjectStatus)),
+        allowNull: false,
+        defaultValue: ObjectStatus.ONLINE,
+      },
+    },
+    {
+      sequelize: db,
+      modelName: "ContactGroup",
+      tableName: "ContactGroup",
+      timestamps: true,
+      paranoid: true,
+    }
+  );
+
+  return ContactGroup;
+};

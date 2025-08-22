d62dd23b55eb0b12a77333009cf7cf1df5d476d1
zaengetsu <106625404+zaengetsu@users.noreply.github.com>
2025-08-22 08:42:47 +0200

    ajout de ;booking_required

--- src/enums/ServiceUptakeType.js
+++ src/enums/ServiceUptakeType.js
@@ -2,6 +2,7 @@ const ServiceUptakeType = {
   INSTANT: 'instant',
   PERIODIC: 'periodic',
   PRESTATION: 'prestation',
+  BOOKING_REQUIRED: 'booking_required',
 };
 
 export default ServiceUptakeType ;
